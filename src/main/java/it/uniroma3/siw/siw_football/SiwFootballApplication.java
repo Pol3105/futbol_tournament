1b7b959b24fe4bb39c9b5a2a4e54c49fe740d43c
Pol3105 <pablorejoncamacho@gmail.com>
2026-04-20 20:27:03 +0200

    chore: initial spring boot setup

---
+++ src/main/java/it/uniroma3/siw/siw_football/SiwFootballApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFootballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFootballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siw_football/SiwFootballApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siw_football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
